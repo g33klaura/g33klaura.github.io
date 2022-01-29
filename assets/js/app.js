cf8981be1bc8448e70798a9310fca58d3a5f204b
Laura Boetcher <g33klaura@gmail.com>
2022-01-28 19:12:03 -0500

    Working out kinks and simplifying the grand vision

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var html = '';
-for (var i = 1; i <= 40; i ++) {
-    html += '<div class="shape-container--'+i+' shape-animation"><div class="random-shape"></div></div>';
-}
+    /*
+    * Logic for Background Funfetti✨
+    * Thank you @sengokukun
+    * https://codepen.io/sengoku/pen/LYVzVjp
+    */
+    var html = '';
+    for (var i = 1; i <= 40; i ++) {
+        html += '<div class="shape-container--'+i+' shape-animation"><div class="random-shape"></div></div>';
+    }
 
-document.querySelector('.shape').innerHTML += html;
+    // document.querySelector('.shape').innerHTML += html;
 
-var $allShapes = $("[class*='shape-container--']");
+    var $allShapes = $("[class*='shape-container--']");
+
+    // Lil asterisks animations
+    $('.fa-asterisk').hover(() => {
+        $('.fa-asterisk').css({
+            'animation': 'fa-spin 2s infinite linear',
+            'color': '#BAFF00',
+        });
+    }, () => {
+        $('.fa-asterisk').css('animation', 'none');
+        $('.thing1').css('color', '#212E31');
+        $('.thing2').css('color', '#E7D6E9');
+    });
 });
