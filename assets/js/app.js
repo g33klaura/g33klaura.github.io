5d58d1973c5c85c6a223f1ee7247e7cd0ac594d8
Laura Boetcher <g33klaura@gmail.com>
2022-02-03 01:42:52 -0500

    WIP logic to render emojis as background elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,14 +5,44 @@ $(document).ready(function () {
     * Thank you @sengokukun
     * https://codepen.io/sengoku/pen/LYVzVjp
     */
+
+    // Keep shape container hidden at first
+    $('.shape').hide();
+
     var html = '';
     for (var i = 1; i <= 40; i ++) {
         html += '<div class="shape-container--'+i+' shape-animation"><div class="random-shape"></div></div>';
     }
 
-    // document.querySelector('.shape').innerHTML += html;
+    document.querySelector('.shape').innerHTML += html;
 
     var $allShapes = $("[class*='shape-container--']");
+
+
+    // to start, add block for each hoverable text
+    // later put that ish in a sweet function
+    $('.hiya').hover(() => {
+        // Add sprite class
+        $('.random-shape').addClass('wavy');
+
+        // Render shape container
+        $('.shape').show();
+    }, () => {
+        // Remove sprite class
+        $('.random-shape').removeClass('wavy');
+
+        // Hide container again
+        $('.shape').hide();
+    });
+
+    $('.poof').hover(() => {
+        $('.random-shape').addClass('genie');
+        $('.shape').show();
+    }, () => {
+        $('.random-shape').removeClass('genie');
+        $('.shape').hide();
+    });
+
 
     // Lil asterisks animations
     $('.fa-asterisk').hover(() => {
